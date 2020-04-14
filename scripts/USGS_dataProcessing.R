b642e647b0c1aa8ebd3e0aec1abff6f7c02d445d
je138 <je138@duke.edu>
2020-04-14 09:42:44 -0400

    Made revisions to data processing and file naming and added additional information to README

--- scripts/USGS_dataProcessing.R
+++ scripts/USGS_dataProcessing.R
@@ -6,9 +6,9 @@ library(stringr)
 library(lubridate)
 
 ## Loading data
-roanoke_rapids_raw <- read.csv(file = "./rawData/RoanokeRapids_USGS_discharge_gageHt_raw.csv")
-oak_city_raw <- read.csv(file = "./rawData/OakCity_USGS_waterquality_raw.csv")
-jamesville_raw <- read.csv(file = "./rawData/Jamesville_USGS_waterquality_raw.csv")
+roanoke_rapids_raw <- read.csv(file = "./Data/rawData/RoanokeRapids_USGS_discharge_gageHt_raw.csv")
+oak_city_raw <- read.csv(file = "./Data/rawData/OakCity_USGS_waterquality_raw.csv")
+jamesville_raw <- read.csv(file = "./Data/rawData/Jamesville_USGS_waterquality_raw.csv")
 
 ## Setting classes
 str(roanoke_rapids_raw)
@@ -26,32 +26,37 @@ jamesville_raw$site_no <- as.factor(jamesville_raw$site_no)
 jamesville_raw$Date <- as.Date(jamesville_raw$Date, format = "%Y-%m-%d")
 jamesville_raw$X_00095_00003 <- as.numeric(jamesville_raw$X_00095_00003)
 
-### Selecting columns and years of interest
-min(jamesville_raw$Date)
+### Selecting columns and years of interest (and removing all observations not "Approved for Publication")
+roanoke_rapids_appr <- roanoke_rapids_raw %>% filter(X_00065_00003_cd == "A" & X_00060_00003_cd == "A")
+min(roanoke_rapids_appr$Date)
+max(roanoke_rapids_appr$Date)
+jamesville_appr <- jamesville_raw %>% filter(X_00065_00003_cd == "A" & X_00010_00003_cd == "A" &
+                                               X_00095_00003_cd == "A" & X_00300_00003_cd == "A")
+min(jamesville_appr$Date)
 max(jamesville_raw$Date)
-min(oak_city_raw$Date)
-max(oak_city_raw$Date)
-min(roanoke_rapids_raw$Date)
-max(roanoke_rapids_raw$Date)
+oak_city_appr <- oak_city_raw %>% filter(X_00065_00003_cd == "A" & X_00010_00003_cd == "A" &
+                                           X_00095_00003_cd == "A" & X_00300_00003_cd == "A")
+min(oak_city_appr$Date)
+max(oak_city_appr$Date)
 
 roanoke_rapids_raw <- roanoke_rapids_raw[,-c(1,2,6,8)] %>% rename(mean.daily.discharge = X_00060_00003)
 roanoke_rapids_raw <- roanoke_rapids_raw %>% rename(gage.height = X_00065_00003)
-roanoke_rapids_processed <- roanoke_rapids_raw %>% filter(Date >= "2007-10-01" & Date <= "2020-04-08")
+roanoke_rapids_processed <- roanoke_rapids_raw %>% filter(Date >= "2007-10-01" & Date <= "2020-02-11")
 
 jamesville_raw <- jamesville_raw[,-c(1,2,6,8,10,12)]
 jamesville_raw <- jamesville_raw %>% rename(gage.height = X_00065_00003)
 jamesville_raw <- jamesville_raw %>% rename(temperature = X_00010_00003)
 jamesville_raw <- jamesville_raw %>% rename(specific.cond = X_00095_00003)
 jamesville_raw <- jamesville_raw %>% rename(DO = X_00300_00003)
-jamesville_processed <- jamesville_raw %>% filter(Date >= "2007-10-01" & Date <= "2020-04-08")
+jamesville_processed <- jamesville_raw %>% filter(Date >= "2007-10-01" & Date <= "2020-02-11")
 
 
 oak_city_raw <- oak_city_raw[,-c(1,2,6,8,10,12)] %>% rename(gage.height = X_00065_00003)
 oak_city_raw <- oak_city_raw %>% rename(temperature = X_00010_00003)
 oak_city_raw <- oak_city_raw %>% rename(specific.cond = X_00095_00003)
 oak_city_raw <- oak_city_raw %>% rename(DO = X_00300_00003)
-oak_city_processed <- oak_city_raw %>% filter(Date >= "2007-10-01" & Date <= "2020-04-08")
+oak_city_processed <- oak_city_raw %>% filter(Date >= "2007-10-01" & Date <= "2020-02-11")
 
-write.csv(roanoke_rapids_processed, file = "./Data/processedData/roanoke_rapids_processed")
-write.csv(jamesville_processed, file = "./Data/processedData/jamesville_processed")
-write.csv(oak_city_processed, file = "./Data/processedData/oak_city_processed")
+write.csv(roanoke_rapids_processed, file = "./Data/processedData/roanoke_rapids_USGS_processed.csv")
+write.csv(jamesville_processed, file = "./Data/processedData/jamesville_USGS_processed.csv")
+write.csv(oak_city_processed, file = "./Data/processedData/oakcity_USGS_processed.csv")
--- scripts/USGS_dataRetrieval.R
+++ scripts/USGS_dataRetrieval.R
@@ -11,6 +11,7 @@ parameterCd <- c("00060","00065")
 raw_Discharge_GageHt <- readNWISdv(siteNumber, parameterCd, "1985-01-01","2020-04-12")
 
 write.csv(raw_Discharge_GageHt, file = "./rawData/RoanokeRapids_USGS_discharge_gageHt_raw.csv")
+write.csv(RoanokeRapids_info, file = "./Data/processedData/roanokerapids_USGS_spatial.csv")
 
 ### Retrieving gage height/DO/specific conductance/pH/temperature for Roanoke River at Jamesville, NC
 siteNumber2 <- "02081094"
@@ -19,6 +20,7 @@ parameterCd2 <- c("00065","00010","00300","00095","00400")
 raw_parameters_Jamesville <- readNWISdv(siteNumber2, parameterCd2, "2007-10-01", "2020-04-13")
 
 write.csv(raw_parameters_Jamesville, file = "./Data/rawData/Jamesville_USGS_waterquality_raw.csv")
+write.csv(Jamesville_info, file = "./Data/processedData/jamesville_USGS_spatial.csv")
 
 ### Retrieving gage height/DO/specific conductance/pH/temperature for Roanoke River near Oak City, NC
 siteNumber3 <- "02081022"
@@ -27,6 +29,7 @@ parameterCd3 <- c("00065","00010","00300","00095","00400")
 raw_parameters_OakCity <- readNWISdv(siteNumber3, parameterCd3, "2007-10-01", "2020-04-08")
 
 write.csv(raw_parameters_OakCity, file = "./Data/rawData/OakCity_USGS_waterquality_raw.csv")
+write.csv(OakCity_info, file = "./Data/processedData/oakcity_USGS_spatial.csv")
 
 str(raw_parameters_Jamesville)
 
