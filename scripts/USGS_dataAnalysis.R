c9c6e4a6709e71d349bb5a2a961b27169707d947
je138 <je138@duke.edu>
2020-04-24 12:40:35 -0400

    worked on data analysis

--- scripts/USGS_dataAnalysis.R
+++ scripts/USGS_dataAnalysis.R
@@ -51,3 +51,42 @@ dunn.test::dunn.test(roanokerapids.discharge$mean.daily.discharge, roanokerapids
 # Based on results of kruskal-wallis test and boxplots, we can conclude that post-QRR discharges are
 # greater than pre-QRR discharges, which is consistent with the QRR regime and our hypthesis.
 
+#### Time series analysis
+## Making time series
+roanokerapids.discharge.ts <- ts(roanokerapids.discharge[[4]], frequency = 365)
+
+## Decomposing time series into components
+roanokerapids.ts.decomposed <- stl(roanokerapids.discharge.ts, s.window = 365)
+
+plot(roanokerapids.ts.decomposed)
+
+## Converting components to data frame
+roanoke.ts.components <- as.data.frame(roanokerapids.ts.decomposed$time.series[,1:3])
+## Adding original data and dates
+roanoke.ts.components <- roanoke.ts.components %>%
+  mutate(original = roanokerapids.discharge$mean.daily.discharge,
+         Date = roanokerapids.discharge$Date)
+
+## Graphing original data with trend
+ggplot(roanoke.ts.components) +
+  geom_line(aes(x= Date, y=original)) +
+  geom_line(aes(x=Date, y=trend), color = "red") +
+  ylab(expression(paste("Discharge (ft"^"3","/s)")))
+
+####### Analyzing effect of discharge on downstream water indicators
+## Combining roanoke rapids and oak city data frames
+rr.oakcity <- left_join(roanokerapids.discharge, oakcity.gage, by = "Date")
+
+## Running regressions
+conductance.regression <- lm(rr.oakcity$specific.cond ~ rr.oakcity$mean.daily.discharge)
+summary(conductance.regression)
+# Coeff has p-value < .05; mean daily discharge is a significant predictor of specific conductance
+temp.regression <- lm(rr.oakcity$temperature ~ rr.oakcity$mean.daily.discharge)
+summary(temp.regression)
+# Coeff has p-value < .05; mean daily discharge is a significant predictor of temperature
+DO.regression <- lm(rr.oakcity$DO ~ rr.oakcity$mean.daily.discharge)
+summary(DO.regression)
+# Coeff has p-value < .05; mean daily discharge is a significant predictor of dissolved oxygen
+## The coefficients for all three regressions were very small, indicating a significant but very weak trend
+
+
